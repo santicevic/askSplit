087ad15d48a55e9529864f389e734270207e456d
sAnticevic <sergijeatnicevic@gmail.com>
2019-08-27 00:32:57 +0200

    Set up for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -12,6 +13,12 @@ app.use(cors());
 
 app.use("/api", routes);
 
-app.listen(8000, () => {
-  console.log("Server started");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
